dc95f75fd2f1533cf11adb1119226d275e0873fe
beniv_bohdan <bohdanbeniv@gmail.com>
2022-06-23 20:55:20 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies/Technologies";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+           <Header/>
+
+            <nav className={"nav"}>
+                <div><a href="#s">Profile</a></div>
+                <div><a href="#s">Messages</a></div>
+                <div><a href="#s">News</a></div>
+                <div><a href="#s">Music</a></div>
+                <div><a href="#s">Settings</a></div>
+            </nav>
+
+            <div className={"content"}>
+                <div>
+                    <img src="https://wallpaper.dog/large/20536905.jpg" alt=""/>
+                </div>
+                <div>ava + desc</div>
+                <div>My posts
+                    <div>New post</div>
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css"
+const Header = () => {
+    return (
+        <div>
+            <header className={"header"}>
+                <img src="https://www.svgrepo.com/show/327388/logo-react.svg" alt=""/>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
