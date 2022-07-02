4b82de51baaf3adf0552a2cb5ad84d04233f339e
beniv_bohdan <bohdanbeniv@gmail.com>
2022-07-03 00:45:54 +0200

    creating a new path & fix the problem related with Routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            {/*<Profile/>*/}
-            <Dialogs/>
-        </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <Routes>
+                    <Route path="/profile" element={<Profile/>}></Route>
+                    <Route path="/dialogs" element={<Dialogs/>}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+            );
 
-    );
-}
+            }
 
-export default App;
+            export default App;
