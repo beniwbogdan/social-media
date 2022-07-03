107efee50b9addb8f387e03f613ee41398be604e
beniv_bohdan <bohdanbeniv@gmail.com>
2022-07-03 10:07:58 +0200

    creating users & dialogs sections (hardcode)

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import d from "./Dialogs.module.css"
-const Dialogs = (props:any) => {
+
+const Dialogs = (props: any) => {
     return (
         <div className={d.dialogs}>
-            <div className={d.users}>USERS</div>
-            <div className={d.chats}>DIALOGS</div>
+            <div className={d.users}><span>USERS:</span>
+                <div className={d.item}>
+                    <div className={d.items}>John</div>
+                    <div className={d.items}>Jerry</div>
+                    <div className={d.items}>Andrea</div>
+                    <div className={d.items}>Lois</div>
+                    <div className={d.items}>Anatoliy</div>
+                </div>
+            </div>
+            <div className={d.chats}><span>DIALOGS:</span>
+                <div className={d.item}>
+                    <div className={d.chat}><img src="https://iconape.com/wp-content/png_logo_vector/react.png" alt=""/>Hello buddy</div>
+                    <div className={d.chat}><img src="https://iconape.com/wp-content/png_logo_vector/react.png" alt=""/>Go to the stroll today? </div>
+                    <div className={d.chat}><img src="https://iconape.com/wp-content/png_logo_vector/react.png" alt=""/>Do you learned something from React today ?</div>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import n from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
 // const n = {
 //     "nav":"Navbar_nav__rfPBT",
 //     "item":"Navbar_item__68eYL",
 //     "active":"Navbar_active__gZRKD"
 // }
-const Navbar = () => {
+const Navbar = (props:any) => {
     return (
 
         <nav className={n.nav}>
-            <div className={n.item}><a href="profile">Profile</a></div>
-            <div className={`${n.item} ${n.active}`}><a href="dialogs">Messages</a></div>
-            <div className={n.item}><a href="#s">News</a></div>
-            <div className={n.item}><a href="#s">Music</a></div>
-            <div className={n.item}><a href="#s">Settings</a></div>
+            <div className={n.item}><NavLink className={n.active} to="profile">Profile</NavLink></div>
+            <div className={`${n.item} ${n.active}`}><NavLink className={n.active} to="dialogs">Messages</NavLink></div>
+            <div className={n.item}><NavLink to="#s">News</NavLink></div>
+            <div className={n.item}><NavLink to="#s">Music</NavLink></div>
+            <div className={n.item}><NavLink to="#s">Settings</NavLink></div>
         </nav>
 
     );
