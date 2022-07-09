e0ba55d357c8949a100d4106969c335c365105f2
beniwbogdan <bohdanbeniv@gmail.com>
2022-07-09 17:42:57 +0200

    trensform data in Dialogs component

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -11,21 +11,30 @@ const DialogItem = (props: any) => {
                                           alt=""/><NavLink to={path}> {props.name}</NavLink></div>
         </>);
 }
-const Message =(props:any)=>{
-return(
-    <div className={d.dialog}><img src="https://iconape.com/wp-content/png_logo_vector/react.png"
-                                    alt=""/>{props.message}
-    </div>
-);
+const Message = (props: any) => {
+    return (
+        <div className={d.dialog}><img src="https://iconape.com/wp-content/png_logo_vector/react.png"
+                                       alt=""/>{props.message}
+        </div>
+    );
 }
 const Dialogs = (props: any) => {
+
+    let dialogData = [
+        {name: "John", id: 1},
+        {name: "Jerry", id: 2},
+        {name: "Andrea", id: 3},
+        {name: "Lois", id: 4},
+        {name: "Anatoliy", id: 5},
+    ]
+
     return (
         <div className={d.dialogs}>
             <div className={d.users}><span>USERS:</span>
                 <div className={d.item}>
-                    <DialogItem name={"John"} id={1}/>
-                    <DialogItem name={"Jerry"} id={2}/>
-                    <DialogItem name={"Andrea"} id={3}/>
+                    <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
+                    <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
+                    <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
                     <DialogItem name={"Lois"} id={4}/>
                     <DialogItem name={"Anatoliy"} id={5}/>
 
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import p from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
-// export const mp = {
-//     "posts": " .MyPosts_posts__2UPfV",
-//     "item": "MyPosts_item__MIc0B",
-//     "postImg":"MyPosts_App-logo__9\+mlU"
-// }
+
 const MyPosts = () => {
     return (
         <div>
