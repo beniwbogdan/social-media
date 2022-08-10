cb483349a93abdd878a1fa7ce49e06c31b88e9e5
Bohdan Beniv <bohdanbeniv@gmail.com>
2022-08-10 16:48:55 +0200

    create addPost & onPostChange action creators functions

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -3,21 +3,29 @@ import p from "./MyPosts.module.css"
 import Post from "./Post/Post";
 export let newPostElementTextarea = React.createRef();
 
+let addPostsActionCreator = () => ({
+    type: "ADD-POST"
+})
+let onPostChangeActionCreator = (text) => ({
+    type: "UPDATE-NEW-POST-TEXT",
+    newText: text
+})
+
 const MyPosts = (props) => {
 
     let newPostElementTextarea = React.createRef();
 
     let addPost = () => {
         //  let text = newPostElementTextarea.current.value;
-        let action = { type: "ADD-POST" };
-        props.dispatch(action);
+        //  let action = { type: "ADD-POST" };
+        props.dispatch(addPostsActionCreator());
 
     }
 
     let onPostChange = () => {
         let text = newPostElementTextarea.current.value;
-        let action = { type: "UPDATE-NEW-POST-TEXT", newText: text };
-        props.dispatch(action);
+        //  let action = { type: "UPDATE-NEW-POST-TEXT", newText: text };
+        props.dispatch(onPostChangeActionCreator(text));
     }
 
     return (
