3feb25e1cd660cc453e1d9d3cd1f2aa504820ada
Bohdan Beniv <bohdanbeniv@gmail.com>
2022-08-26 11:35:42 +0200

    create axios get request on api server https://social-network.samuraijs.com/api/1.0/ and use some another endpoints

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,21 +1,32 @@
-import { v1 } from "uuid";
+import * as axios from "axios";
 import styles from "./users.module.css"
+import userPhoto from "../../assets/user.png"
+
 let Users = (props) => {
+
+    // if (props.users.length === 0) {
+    //     props.setUsers([
+    //         { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: true, fullName: "Name S.", status: "Who i am", location: { city: "Krakow", country: "Polska" } },
+    //         { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: true, fullName: "Name S.", status: "Work hard", location: { city: "Warshaw", country: "Polska" } },
+    //         { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: false, fullName: "Name S.", status: "Procrastination is not about me", location: { city: "Wroclaw", country: "Polska" } },
+    //         { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: true, fullName: "Name S.", status: "Looking for job", location: { city: "Wieliczka", country: "Polska" } },
+    //     ]);
+    // }
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: true, fullName: "Name S.", status: "Who i am", location: { city: "Krakow", country: "Polska" } },
-            { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: true, fullName: "Name S.", status: "Work hard", location: { city: "Warshaw", country: "Polska" } },
-            { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: false, fullName: "Name S.", status: "Procrastination is not about me", location: { city: "Wroclaw", country: "Polska" } },
-            { id: v1(), photoUrl: "https://aux2.iconspalace.com/uploads/41783629.png", followed: true, fullName: "Name S.", status: "Looking for job", location: { city: "Wieliczka", country: "Polska" } },
-        ]);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            })
     }
-    return <div>
+
+    return <div className={styles.profileUsersPage}>
         {
             props.users.map(u => <div key={u.id}>
-                <span>
-                    <div ><img src={u.photoUrl} className={styles.userPhoto} alt="User logo without img" /></div>
-                    <div>
-                        <button>
+                <span className={styles.userItem}>
+                    <div ><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="User logo without img" /></div>
+                    <div >
+                        <button className={styles.followButton}>
                             {
                                 u.followed
                                     ? <button onClick={() => { props.unfollow(u.id) }}>followed</button>
@@ -26,12 +37,14 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"Krakow"}</div>
+                        <div>{"Poland"}</div>
+                        {/* <div>{u.location.country}</div>
+                        <div>{u.location.city}</div> */}
                     </span>
                 </span>
             </div>)
