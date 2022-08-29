247ed4e1e3b21b7b71b66ae100125024002b508d
Bohdan Beniv <bohdanbeniv@gmail.com>
2022-08-29 15:12:46 +0200

    presentation & container components

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,49 +1,52 @@
-import * as axios from "axios";
 import styles from "./users.module.css"
 import userPhoto from "../../assets/user.png"
 
-let Users = (props) => {
+function Users(props) {
+    let pagesCount = Math.ceil((props.totalUsersCount) / (props.pageSize));
+    let pages = [];
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    props.setUsers(response.data.items);
-                })
-        }
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
-    return <div className={styles.profileUsersPage}>
-        <button onClick={getUsers}>get users</button>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span className={styles.userItem}>
-                    <div ><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="User logo without img" /></div>
-                    <div >
-                        <button className={styles.followButton}>
-                            {
-                                u.followed
-                                    ? <button onClick={() => { props.unfollow(u.id) }}>followed</button>
-                                    : <button onClick={() => { props.follow(u.id) }}>unfollowed</button>
-                            }
-                        </button>
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{"Krakow"}</div>
-                        <div>{"Poland"}</div>
-                        {/* <div>{u.location.country}</div>
-                        <div>{u.location.city}</div> */}
-                    </span>
-                </span>
-            </div>)
-        }
-    </div>
-}
 
+    return (
+        <div className={styles.profileUsersPage}>
+            <div className={styles.numbersOfPage}>
+                {pages.map(p => {
+                    return <span
+                        className={props.currentPage === p && styles.selectedPage}
+                        onClick={(e) => props.onPageChanged(p)}>{p}</span>
+                })}
+            </div>
+            {
+                props.users.map(u => <div key={u.id}>
+                    <span className={styles.userItem}>
+                        <div ><img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="User logo without img" /></div>
+                        <div >
+                            <button className={styles.followButton}>
+                                {
+                                    u.followed
+                                        ? <button onClick={() => { props.unfollow(u.id) }}>followed</button>
+                                        : <button onClick={() => { props.follow(u.id) }}>unfollowed</button>
+                                }
+                            </button>
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"Krakow"}</div>
+                            <div>{"Poland"}</div>
+                            {/* <div>{u.location.country}</div>
+                    <div>{u.location.city}</div> */}
+                        </span>
+                    </span>
+                </div>)
+            }
+        </div>
+    )
+}
 export default Users
---
+++ src/Components/Users/UsersContainer.jsx
@@ -0,0 +1,53 @@
+import { followActionCreator, unfollowActionCreator, setUsersActionCreator, setCurrentPageActionCreator, setUsersTotalCountActionCreator } from "../../redux/user-reducer";
+import React from 'react';
+import { connect } from 'react-redux';
+import * as axios from "axios";
+import Users from './Users';
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            })
+
+    }
+
+    render() {
+        return (<Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+        />);
+    }
+}
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
+    }
+}
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userID) => { dispatch(followActionCreator(userID)) },
+        unfollow: (userID) => { dispatch(unfollowActionCreator(userID)) },
+        setUsers: (users) => { dispatch(setUsersActionCreator(users)) },
+        setCurrentPage: (pageNumber) => { dispatch(setCurrentPageActionCreator(pageNumber)) },
+        setTotalUsersCount: (totalCount) => { dispatch(setUsersTotalCountActionCreator(totalCount)) }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
