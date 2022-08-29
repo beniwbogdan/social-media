34cbbe383d99a364ba9ad156835ea949ea6ea9fc
Bohdan Beniv <bohdanbeniv@gmail.com>
2022-08-29 16:51:33 +0200

    #57 preloader

--- src/redux/user-reducer.js
+++ src/redux/user-reducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW ="UNFOLLOW";
 const SET_USERS="SET_USERS";
 const SET_CURRENT_PAGE="SET-CURRENT-PAGE";
 const SET_TOTAL_USERS_COUNT="SET-TOTAL-USERS-COUNT";
+const TOGGLE_IS_FETCHING="TOGGLE_IS_FETCHING";
 
 let initialState ={
     users: [],
     pageSize:5,
     totalUsersCount:0,
     currentPage:1,
+    isFetching: true,
 }
  const usersReducer=(state=initialState, action)=>{
 
@@ -41,6 +43,8 @@ let initialState ={
                 return{...state, currentPage:action.currentPage}
             case SET_TOTAL_USERS_COUNT:
                 return{...state,totalUsersCount:action.count>50?action.count=50:action.count }
+            case TOGGLE_IS_FETCHING:
+                return{...state, isFetchin:action.isFetching}
         default: return state;
     }
 }
@@ -66,5 +70,9 @@ export const setCurrentPageActionCreator = (currentPage) => ({
 export const setUsersTotalCountActionCreator = (totalUsersCount) => ({
     type: SET_TOTAL_USERS_COUNT,
     count:totalUsersCount
+})
+export const toggleIsFetchingActionCreator = (isFetching) => ({
+    type: TOGGLE_IS_FETCHING,
+    isFetching
 })
 export default usersReducer
