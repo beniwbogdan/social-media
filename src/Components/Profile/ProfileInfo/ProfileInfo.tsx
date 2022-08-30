d23cf28043d1e9ca54acd464fad532f70a184d94
beniwbogdan <bohdanbeniv@gmail.com>
2022-08-30 19:26:58 +0200

    fixed problem rlated with loader

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -14,7 +14,7 @@ const ProfileInfo = (props: any) => {
             </div>
             <div className={p.descriptionBlock}>
                 <img alt='profile pic' src={props.profile.photos.large ? props.profile.photos.large : userPhoto} /><br />
-                <span><b>Name :</b> {props.profile.fullName}</span><br />
+                <span><b>Name :</b>  {props.profile.fullName}</span><br />
                 <span><b>Describtion :</b> {props.profile.lookingForAJobDescription}</span><br />
                 <span><b>Looking for job :</b>{props.profile.lookingForAJob ? " Yes" : " No, i`m working now"}</span><br />
                 <span><b>Contacts:</b> </span>
@@ -22,7 +22,7 @@ const ProfileInfo = (props: any) => {
                     <li><b>facebook : </b>{props.profile.contacts.facebook ? props.profile.contacts.facebook : "No have any links"}</li>
                     <li><b>twitter : </b>{props.profile.contacts.twitter ? props.profile.contacts.twitter : "No have any links"}</li>
                     <li><b>instagram : </b>{props.profile.contacts.instagram ? props.profile.contacts.instagram : "No have any links"}</li>
-                    <li><b>github : </b>{props.profile.contacts.github ? props.profile.contacts.github : "No have any links"}</li>
+                    <li><b>github : </b> {props.profile.contacts.github ? props.profile.contacts.github : "No have any links"}</li>
                     <li><b>website : </b>{props.profile.contacts.website ? props.profile.contacts.website : "No have any links"}</li>
                 </ul>
             </div>
